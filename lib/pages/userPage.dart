b99abb05137b565de2da46e77ba691a4496dc9af
Ismael Osuna <ismael.osuna@umvel.com>
2022-11-24 20:21:26 -0700

    main

--- lib/pages/userPage.dart
+++ lib/pages/userPage.dart
@@ -38,101 +38,94 @@ class _UserPageState extends State<UserPage> {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: Container(
-        child: Form(
-          key: _fomkey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                    icon: Icon(Icons.person),
-                    hintText: 'Enter your full name',
-                    labelText: 'Name'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: lastNameController,
-                decoration: const InputDecoration(
-                    icon: Icon(Icons.person),
-                    hintText: 'Enter your full lastname',
-                    labelText: 'lastname'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 60),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () async {
-                            if (_fomkey.currentState!.validate()) {
-                              if (iduser == 0) {
-                                ApiUser.createUser(User(
-                                        name: nameController.value.text,
-                                        id: iduser,
-                                        lastname:
-                                            lastNameController.value.text))
-                                    .then((value) {
-                                  var resp = json.decode(value.body);
-                                  if (resp['status']) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        snackbar('user added successfully'));
-                                    Navigator.pushReplacementNamed(
-                                        context, '/');
-                                  }
-                                });
-                              } else {
-                                ApiUser.updateUser(User(
-                                        name: nameController.value.text,
-                                        id: iduser,
-                                        lastname:
-                                            lastNameController.value.text))
-                                    .then((value) {
-                                  var resp = json.decode(value.body);
-                                  if (resp['status']) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        snackbar('user updated successfully'));
-                                    Navigator.pushReplacementNamed(
-                                        context, '/');
-                                  }
-                                });
-                              }
-                              //print(nameController.value.text);
-                            } else {
-                              // print("faltan campos por llenar");
-                              // print(nameController.value);
+      body: Form(
+        key: _fomkey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Enter your full name',
+                  labelText: 'Name'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: lastNameController,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Enter your full lastname',
+                  labelText: 'lastname'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.only(top: 60),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_fomkey.currentState!.validate()) {
+                        if (iduser == 0) {
+                          ApiUser.createUser(User(
+                                  name: nameController.value.text,
+                                  id: iduser,
+                                  lastname: lastNameController.value.text))
+                              .then((value) {
+                            var resp = json.decode(value.body);
+                            if (resp['status']) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  snackbar('user added successfully'));
+                              Navigator.pushReplacementNamed(context, '/');
                             }
+                          });
+                        } else {
+                          ApiUser.updateUser(User(
+                                  name: nameController.value.text,
+                                  id: iduser,
+                                  lastname: lastNameController.value.text))
+                              .then((value) {
+                            var resp = json.decode(value.body);
+                            if (resp['status']) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  snackbar('user updated successfully'));
+                              Navigator.pushReplacementNamed(context, '/');
+                            }
+                          });
+                        }
+                        //print(nameController.value.text);
+                      } else {
+                        // print("faltan campos por llenar");
+                        // print(nameController.value);
+                      }
+                    },
+                    child: Text(iduser == 0 ? 'Guardar' : 'Actualizar'),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  iduser != 0
+                      ? ElevatedButton(
+                          onPressed: () {
+                            showAlertDialog(context, iduser);
                           },
-                          child: Text(iduser == 0 ? 'Guardar' : 'Actualizar'),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        iduser != 0
-                            ? ElevatedButton(
-                                onPressed: () {
-                                  showAlertDialog(context, iduser);
-                                },
-                                child: const Text('Eliminar'))
-                            : Container()
-                      ]),
-                ),
-              )
-            ],
-          ),
+                          child: const Text('Eliminar'))
+                      : Container()
+                ]),
+              ),
+            )
+          ],
         ),
       ),
     );
